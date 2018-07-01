d9d496a3ce30d64af0b038f266403c0a6d166c92
Mohammedalhady Al Edreesi <edreesmm@hotmail.com>
2018-07-02 02:26:32 +0300

    fouur plots for push

---
+++ plot1.R
@@ -0,0 +1,17 @@
+datafile <- "C:/Users/DEll/OneDrive/Coursera/EXPLORATORY DATA ANALYSIS/WEEK 1 PROJECT,PLOTS/household_power_consumption.txt"
+
+data <- read.table(datafile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+
+
+#str(subSetData)
+
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+
+png("plot1.png", width=480, height=480)
+
+hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+
+dev.off()
